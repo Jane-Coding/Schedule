afb9b3d71e595cd9c667282f35ec6aa07d508a11
Jane Doe <raidho27@gmail.com>
2026-02-26 15:57:37 +0300

    first setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Container from '@mui/material/Container';
+import DenseTable from './DenseTable'
+
+function App() {
+
+  return (
+    <>
+    <Container maxWidth="sm">
+      <DenseTable></DenseTable>
+    </Container>      
+    </>
+  )
+}
+
+export default App
